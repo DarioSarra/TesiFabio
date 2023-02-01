0cb964a4b8660f70aa776e4e46d6e2452bb040ea
DarioSarra <dariosarra@gmail.com>
2023-02-01 14:39:14 +0000

    VSCode

--- src/find_frame.jl
+++ src/find_frame.jl
@@ -10,9 +10,9 @@ movie = VideoIO.openvideo(path)
 movie color array we need to account for the frames remove at the start of the
 movie. The seek function  search using seconds as input. SO to find a specific
 frame you need to convert the index with bin_size_in_seconds and add 13s=#
-seek(movie,framevalues("0h:10m:05s"))
+seek(movie,framevalues("0h:20m:05s"))
     screenshot = read(movie)
-read(movie)
+    
 
 save(joinpath(cartella,"party1.png"), screenshot) #tempo "0h:10m:05s"
 save(joinpath(cartella,"party2.png"), screenshot) #tempo "0h:19m:03s"
